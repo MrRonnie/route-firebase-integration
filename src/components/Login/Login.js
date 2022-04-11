2bbfccf2c7efb9d79055c2149c992249699a4381
MrRonnie <ronymaza55@gmail.com>
2022-04-11 16:55:39 +0600

    Install React Firebase Hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import userFirebase from "../../hooks/useFirebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
 
 const Login = () => {
-  const { singInWithGoogle } = userFirebase();
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
   return (
     <div>
       <h2>Please Login</h2>
       <div style={{ margin: "20px" }}>
-        <button onClick={singInWithGoogle}>Google Sing In</button>
+        <button onClick={() => signInWithGoogle()}>Google Sing In</button>
       </div>
       <form>
         <input type="email" placeholder="Your Email" />
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,14 @@
 import React from "react";
 import useFirebase from "../../hooks/useFirebase";
 
-const Home = () => {
+const Products = () => {
   const { user } = useFirebase();
   return (
     <div>
-      <h2>This is home</h2>
-      <p>Current user is: {user ? user.displayName : "No user is Active"}</p>
+      <h2>Knock Knock ! who is there</h2>
+      <h5>{user ? user.displayName : "Ami Vhoot na!"}</h5>
     </div>
   );
 };
 
-export default Home;
+export default Products;
